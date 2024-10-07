3e3adc51a7e827b1b5bdfac8dc15f586646fd6b3
Zach Donnelly <zachary.donnelly@walmart.com>
2024-10-07 13:06:29 -0400

    feat: add product page + server action set up

---
+++ app/add-product/page.tsx
@@ -0,0 +1,75 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "Add Product - Zhopify",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  });
+
+  redirect("/");
+}
+
+const AddProductPage: React.FC = () => {
+  return (
+    <div className="w-full px-16 flex flex-col justify-center items-center">
+      <h1 className="mb-3 text-lg font-bold underline">Add Product</h1>
+      <form action={addProduct} className="w-[500px] flex flex-col">
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input-bordered input mb-3 w-full border-2 border-black rounded-md pl-2"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full border-2 border-black rounded-md pl-2"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input-bordered input mb-3 w-full border-2 border-black rounded-md pl-2"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input-bordered input mb-3 w-full border-2 border-black rounded-md pl-2"
+        />
+        <button
+          type="submit"
+          className="self-center border-2 p-3 border-black bg-slate-200 rounded-md font-semibold cursor-pointer"
+        >
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import { siteConfig } from '@/lib/constants/config';
-import Footer from '@/ui/components/layout/footer';
-import Header from '@/ui/components/layout/header';
-import '@/ui/styles/global.css';
-import type { Metadata } from 'next';
+import { siteConfig } from "@/lib/constants/config";
+import Footer from "@/ui/components/layout/footer";
+import Header from "@/ui/components/layout/header";
+import "@/ui/styles/global.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = siteConfig;
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -12,10 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body>
+    <html lang="en">
+      <body className={inter.className}>
         <Header />
-        <main className='main'>{children}</main>
+        <main className="min-w-[300px]">{children}</main>
         <Footer />
       </body>
     </html>
