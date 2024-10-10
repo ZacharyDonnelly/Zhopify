0dd592f8f854c2da174a23bd7f002a12dff29d0c
Zach Donnelly <zachary.donnelly@walmart.com>
2024-10-09 21:02:18 -0400

    feat: add cart page

--- app/cart/CartEntry.tsx
+++ app/cart/CartEntry.tsx
@@ -1,18 +1,70 @@
 "use client";
 
-// import { CartItemWithProduct } from "@/lib/db/cart";
+import NextImage from "@/ui/components/nextImage";
+
+import { CartItemWithProduct } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/utils/format";
+import Link from "next/link";
+import { useTransition } from "react";
 
 interface CartEntryProps {
-  // cartItem: CartItemWithProduct;
+  cartItem: CartItemWithProduct;
   setProductQuantity: (productId: string, quantity: number) => Promise<void>;
 }
 const CartEntry: React.FC<CartEntryProps> = ({
-  // cartItem: { product, quantity },
+  cartItem: { product, quantity },
   setProductQuantity,
 }) => {
+  const [isPending, startTransition] = useTransition();
+
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
   return (
     <div>
-      <h1>test</h1>
+      <div className="flex flex-wrap items-center gap-3">
+        <NextImage
+          src={product.imageUrl}
+          alt={product.name}
+          width={200}
+          height={200}
+          className="rounded-lg"
+        />
+        <div>
+          <Link href={"/products/" + product.id} className="font-bold">
+            {product.name}
+          </Link>
+          <div>Price: {formatPrice(product.price)}</div>
+          <div className="my-1 flex items-center gap-2">
+            Quantity:
+            <select
+              className="select-bordered select w-full max-w-[80px]"
+              defaultValue={quantity}
+              onChange={(e) => {
+                const newQuantity = parseInt(e.currentTarget.value);
+                startTransition(async () => {
+                  await setProductQuantity(product.id, newQuantity);
+                });
+              }}
+            >
+              <option value={0}>0 (Remove)</option>
+              {quantityOptions}
+            </select>
+          </div>
+          <div className="flex items-center gap-3">
+            Total: {formatPrice(product.price * quantity)}
+            {isPending && (
+              <span className="loading loading-spinner loading-sm" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="divider" />
     </div>
   );
 };
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,7 @@
+import { setProductQuantity } from "@/cart/actions";
+import CartEntry from "@/cart/CartEntry";
 import { getCart } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/utils/format";
 
 export const metadata = {
   title: "Your Cart - Zhopify",
@@ -8,7 +11,21 @@ const CartPage: React.FC = async () => {
   const cart = await getCart();
   return (
     <div>
-      <h1>Cart Page</h1>
+      <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
+      {cart?.items.map((cartItem) => (
+        <CartEntry
+          cartItem={cartItem}
+          key={cartItem.id}
+          setProductQuantity={setProductQuantity}
+        />
+      ))}
+      {!cart?.items.length && <p>Your cart is empty.</p>}
+      <div className="flex flex-col items-end sm:items-center">
+        <p className="mb-3 font-bold">
+          Total: {formatPrice(cart?.subtotal || 0)}
+        </p>
+        <button className="btn-primary btn sm:w-[200px]">Checkout</button>
+      </div>
     </div>
   );
 };
